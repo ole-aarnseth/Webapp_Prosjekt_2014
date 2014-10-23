71adf335e4f9bf3e2ae01681d668587a62f8808b
ahmedaw114 <mxamedcw@hotmail.com>
2014-10-23 18:12:39 +0200

    lagt til prosjekt enhetstest med references og samtidig opprettet AdminController. oprette en tom klasse inAdminLogic i BLL. opprettet tomme klassene InAdminRepository, AdminRepository og AdminRepository i DAL .

---
+++ UnitTest/AdminControllertest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prosjekt1.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class AdminControllertest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new AdminController();
+        }
+    }
+}
