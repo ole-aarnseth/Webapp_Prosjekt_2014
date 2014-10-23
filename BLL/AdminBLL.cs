d87909a0a45b8cb0a6ea9dfb2535c5094a754271
ole.aarnseth <s180482@stud.hioa.no>
2014-10-23 18:10:12 +0200

    La til Admin Sign In og Admin-meny.

--- BLL/AdminBLL.cs
+++ BLL/AdminBLL.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using Model;
+using System.Web;
 
 namespace BLL
 {
@@ -18,6 +20,45 @@ namespace BLL
         public static byte[] GeneratePasswordHash(string Password)
         {
             return AdminDAL.GeneratePasswordHash(Password);
+        }
+
+        public bool SignIn(HttpContextBase Context, string Email, string Password)
+        {
+            AdminDAL dal = new AdminDAL();
+            var admin = dal.GetAdmin(Email, Password);
+
+            if (admin != null)
+            {
+                Context.Session[Constants.AdminSignedInSessionKey] = admin.AdminId.ToString();
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public Admin GetAdmin(int AdminId)
+        {
+            AdminDAL dal = new AdminDAL();
+            return dal.GetAdmin(AdminId);
+        }
+
+        public Admin GetSignedInAdmin(HttpContextBase Context)
+        {
+            if (Context.Session[Constants.AdminSignedInSessionKey] != null)
+            {
+                AdminDAL dal = new AdminDAL();
+                int AdminId = Convert.ToInt32(Context.Session[Constants.AdminSignedInSessionKey]);
+
+                return dal.GetAdmin(AdminId);
+            }
+
+            else
+            {
+                return null;
+            }
         }
     }
 }
--- Prosjekt1/Controllers/AdminController.cs
+++ Prosjekt1/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Prosjekt1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,29 @@ namespace Prosjekt1.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        public ActionResult SignIn()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(SignInCredentials creds)
+        {
+            if (ModelState.IsValid)
+            {
+                AdminBLL bll = new AdminBLL();
+
+                if (bll.SignIn(this.HttpContext, creds.Email, creds.LoginPassword))
+                {
+                    return RedirectToAction("Index", "Store");
+                }
+
+                else
+                {
+                    ViewBag.SignInFailed = "Invalid email or password";
+                }
+            }
+
             return View();
         }
     }
